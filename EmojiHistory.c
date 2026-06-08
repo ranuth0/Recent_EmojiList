68afdb7bdbe469038966015337ad70cc2e17ec5f
ranuth0 <ratharanuthaun@gmail.com>
2026-06-08 16:40:41 +0700

    Added the function prototypes and main.c and defined length of the array

---
+++ EmojiHistory.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+//Let the emojilist store 10 emojis
+#define Length_Name 50
+#define Emoji_List 10
+
+int finditem();
+int removeDuplicate();
+void shiftRight();
+void insertFront();
+void displayHistory();
+void saveHistory();
+int loadHistory();
+
+int main() {
+
+    return 0;
+}
