9ddb59e04b38794d0e95657f5abc052b67b62e95
Yousthoninsakd Rith <yousthoninsakdrith@gmail.com>
2026-07-01 16:09:11 -0400

    LoadHistory.c

--- EmojiHistory.c
+++ EmojiHistory.c
@@ -45,10 +45,17 @@ void saveHistory() {
 
 
 // Reads saved emoji strings from the .txt file back into history_array
-//Sakd
 int loadHistory() {
     // TODO: fopen("history.txt", "r"), fscanf() strings into array, return count
-    return 0; 
+    FILE *fp;
+    fp = fopen("history.txt", "w");
+    int current_count = 0; 
+    while (fscanf(fp, "%s", history_array[current_count]))
+    {
+        current_count++;
+    }
+    fclose(fp);
+    return current_count; 
 }
 
 // Searches for a specific emoji string in the array
